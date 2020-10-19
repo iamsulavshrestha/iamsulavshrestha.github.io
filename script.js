72d473752b72f632a7575726dbef576094120520
iamsulavshrestha <iamsulavshrestha@gmail.com>
2020-10-19 20:56:47 +0545

    added gravatar

---
+++ script.js
@@ -0,0 +1,15 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAcH5Jis8tZx_3tzSBbu2mhmEJx5afckCc",
+    authDomain: "sulav-shrestha-62ac3.firebaseapp.com",
+    databaseURL: "https://sulav-shrestha-62ac3.firebaseio.com",
+    projectId: "sulav-shrestha-62ac3",
+    storageBucket: "sulav-shrestha-62ac3.appspot.com",
+    messagingSenderId: "93754731902",
+    appId: "1:93754731902:web:36eee048f4b82b73f7d72f",
+    measurementId: "G-2YGV4EVX7G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
